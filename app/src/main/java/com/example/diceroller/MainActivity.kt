bec20e887f6cf72549cb09bd888f327c8619681b
NaoUeda12 <ueda.nao@chrono.co.jp>
2024-10-09 11:54:33 +0900

    ボタン上にテキスト追加

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,6 +68,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Image(painter = painterResource(imageResource), contentDescription = result.toString())
 
+        Text(
+            text = stringResource(R.string.tap_button),
+            fontSize = 15.sp,
+            )
+        Spacer(modifier = Modifier.height(14.dp))
         Button(
             onClick = { result = (1..6).random()
                 println("Dice rolled: $result")
